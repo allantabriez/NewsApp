1b5f8ccfd715a564d6502c125941a2da8b7e9613
Allan Tabriez <allantabriez@gmail.com>
2021-10-07 19:16:59 +0700

    Added AuthInterceptor.kt

--- app/src/main/java/com/example/newsapp/Application.kt
+++ app/src/main/java/com/example/newsapp/Application.kt
@@ -3,11 +3,13 @@ package com.example.newsapp
 import android.app.Application
 import com.example.newsapp.di.networkModule
 import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class Application: Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
+            printLogger(Level.INFO)
             networkModule
         }
     }
--- app/src/main/java/com/example/newsapp/di/AppModules.kt
+++ app/src/main/java/com/example/newsapp/di/AppModules.kt
@@ -1,17 +1,21 @@
 package com.example.newsapp.di
 
 import com.example.newsapp.data.remote.network.ApiService
+import com.example.newsapp.data.remote.network.AuthInterceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 val networkModule = module {
+    single { AuthInterceptor() }
     single {
         OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .addInterceptor(get<AuthInterceptor>())
             .connectTimeout(30, TimeUnit.SECONDS)
             .connectTimeout(30, TimeUnit.SECONDS)
             .build()
