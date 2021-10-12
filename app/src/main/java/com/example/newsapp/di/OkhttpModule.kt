98fb69d24ebb1b37a767a61be0fbe253c24e595a
Allan Tabriez Rosyada <allantabriez98@mail.ugm.ac.id>
2021-10-12 17:27:23 +0700

    Logging okhttp only on debug

--- app/src/main/java/com/example/newsapp/di/OkhttpModule.kt
+++ app/src/main/java/com/example/newsapp/di/OkhttpModule.kt
@@ -1,6 +1,8 @@
 package com.example.newsapp.di
 
+import com.example.newsapp.BuildConfig
 import com.example.newsapp.data.remote.network.AuthInterceptor
+import com.example.newsapp.utils.Constants.BUILD_TYPE_DEBUG
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
@@ -8,11 +10,17 @@ import java.util.concurrent.TimeUnit
 
 val okhttpModule = module {
     single {
-        OkHttpClient.Builder()
-            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+        val okhttp = OkHttpClient.Builder()
             .addInterceptor(get<AuthInterceptor>())
             .connectTimeout(30, TimeUnit.SECONDS)
             .readTimeout(30, TimeUnit.SECONDS)
-            .build()
+
+        if (BuildConfig.BUILD_TYPE == BUILD_TYPE_DEBUG) {
+            okhttp.addInterceptor(
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+            )
+        }
+
+        okhttp.build()
     }
 }
--- app/src/main/java/com/example/newsapp/utils/Constants.kt
+++ app/src/main/java/com/example/newsapp/utils/Constants.kt
@@ -6,4 +6,5 @@ object Constants {
     const val PROFILE_URL = "me/profile"
     const val NEWS_URL = "me/news"
     const val JSON_MEDIA_TYPE = "application/json"
+    const val BUILD_TYPE_DEBUG = "debug"
 }
