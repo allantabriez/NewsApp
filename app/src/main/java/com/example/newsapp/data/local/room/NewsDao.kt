798bf946db2d0c4e8b6120bc017d28e9b8fae74c
Allan Tabriez Rosyada <allantabriez98@mail.ugm.ac.id>
2021-10-14 16:16:13 +0700

    Bugfix Room Dao's

--- app/src/main/java/com/example/newsapp/data/local/room/NewsDao.kt
+++ app/src/main/java/com/example/newsapp/data/local/room/NewsDao.kt
@@ -1,11 +1,13 @@
 package com.example.newsapp.data.local.room
 
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.newsapp.data.local.entity.NewsEntity
 import kotlinx.coroutines.flow.Flow
 
+@Dao
 interface NewsDao {
 
     @Insert(entity = NewsEntity::class, onConflict = OnConflictStrategy.REPLACE)
--- app/src/main/java/com/example/newsapp/data/local/room/ProfileDao.kt
+++ app/src/main/java/com/example/newsapp/data/local/room/ProfileDao.kt
@@ -1,11 +1,13 @@
 package com.example.newsapp.data.local.room
 
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.newsapp.data.local.entity.ProfileEntity
 import kotlinx.coroutines.flow.Flow
 
+@Dao
 interface ProfileDao {
 
     @Insert(entity = ProfileEntity::class, onConflict = OnConflictStrategy.REPLACE)
