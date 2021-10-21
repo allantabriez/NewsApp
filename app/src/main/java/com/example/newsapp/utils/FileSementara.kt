709325363dacaa32efb89fa80cae6b44351ced52
allantabriez <allantabriez@gmail.com>
2021-10-21 10:30:14 +0700

    Added descrption for FileSementara.kt

--- app/src/main/java/com/example/newsapp/utils/FileSementara.kt
+++ app/src/main/java/com/example/newsapp/utils/FileSementara.kt
@@ -1,5 +1,8 @@
 package com.example.newsapp.utils
 
+// This file will be deleted, because it already exist in another branch that I'm currently asking
+// for review, so I haven't merged those branches yet
+
 sealed class Resource<T>(val data: T? = null, val message: ErrorCode? = null) {
     class Success<T>(data: T) : Resource<T>(data)
     class Error<T>(code: ErrorCode, data: T? = null) : Resource<T>(data, code)
