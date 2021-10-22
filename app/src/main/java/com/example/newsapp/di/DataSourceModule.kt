4e1a46c685c1a296a19bf8b92e437bcba821fb5a
allantabriez <allantabriez@gmail.com>
2021-10-22 17:59:57 +0700

    Changed androidContext to androidApplication

--- app/src/main/java/com/example/newsapp/di/DataSourceModule.kt
+++ app/src/main/java/com/example/newsapp/di/DataSourceModule.kt
@@ -5,12 +5,13 @@ import com.example.newsapp.data.local.SessionManager
 import com.example.newsapp.data.remote.LoginRemoteDataSource
 import com.example.newsapp.data.remote.MeRemoteDataSource
 import kotlinx.coroutines.Dispatchers
+import org.koin.android.ext.koin.androidApplication
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val dataSourceModule = module {
     single { LoginRemoteDataSource(get(), Dispatchers.IO) }
     single { MeRemoteDataSource(get(), Dispatchers.IO) }
-    single { SessionManager(androidContext()) }
+    single { SessionManager(androidApplication()) }
     single { MeLocalDataSource(get(), get(), Dispatchers.IO) }
 }
--- app/src/main/java/com/example/newsapp/di/RoomModule.kt
+++ app/src/main/java/com/example/newsapp/di/RoomModule.kt
@@ -3,12 +3,13 @@ package com.example.newsapp.di
 import androidx.room.Room
 import com.example.newsapp.data.local.room.AppDatabase
 import com.example.newsapp.utils.Constants.DB_NAME
+import org.koin.android.ext.koin.androidApplication
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val roomModule = module {
     single {
-        Room.databaseBuilder(androidContext(), AppDatabase::class.java, DB_NAME)
+        Room.databaseBuilder(androidApplication(), AppDatabase::class.java, DB_NAME)
             .fallbackToDestructiveMigration().build()
     }
     factory { get<AppDatabase>().newsDao() }
