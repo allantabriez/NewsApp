381d8010e958e4a7e5429a6d31962f9f52f76541
allantabriez <allantabriez@gmail.com>
2021-10-30 18:54:15 +0700

    refactor home error

--- app/src/main/java/com/example/newsapp/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/example/newsapp/presentation/home/HomeScreen.kt
@@ -1,8 +1,8 @@
 package com.example.newsapp.presentation.home
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -10,7 +10,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.annotation.ExperimentalCoilApi
+import com.example.newsapp.R
 import com.example.newsapp.domain.model.News
 import com.example.newsapp.domain.model.Profile
 import com.example.newsapp.presentation.home.composables.LoginDialog
@@ -67,12 +72,23 @@ fun HandleLoading() {
 
 @Composable
 fun DisplayError(){
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
     ) {
         Text(
-            text = "Sorry, something went wrong.. Please try again later",
+            text = "Oops.. Sorry!",
+            style = MaterialTheme.typography.h1.copy(
+                fontSize = 18.sp,
+                letterSpacing = 0.5.sp,
+                fontWeight = FontWeight.Medium
+            )
+        )
+        Text(
+            text = stringResource(id = R.string.something_went_wrong),
         )
     }
 }
