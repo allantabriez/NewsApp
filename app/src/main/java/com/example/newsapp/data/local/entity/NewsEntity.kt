19c8f1d0ccfb1064c30cf80a61d33800c81edc55
allantabriez <allantabriez@gmail.com>
2021-11-01 16:37:20 +0700

    Add entity to model function, optimized imports in local data layer

--- app/src/main/java/com/example/newsapp/data/local/entity/NewsEntity.kt
+++ app/src/main/java/com/example/newsapp/data/local/entity/NewsEntity.kt
@@ -3,6 +3,7 @@ package com.example.newsapp.data.local.entity
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.example.newsapp.domain.model.News
 
 @Entity(tableName = "news_table")
 data class NewsEntity(
@@ -11,32 +12,48 @@ data class NewsEntity(
     val id: Int,
 
     @ColumnInfo(name = "title")
-    val title: String?,
+    val title: String,
 
     @ColumnInfo(name = "url")
-    val url: String?,
+    val url: String,
+
+    @ColumnInfo(name = "cover_image")
+    val coverImg: String,
 
     @ColumnInfo(name = "nsfw")
     val nsfw: Boolean,
 
     @ColumnInfo(name = "created_at")
-    val createdAt: String?,
+    val createdAt: String,
 
     @ColumnInfo(name = "channel_id")
-    val channelId: Int?,
+    val channelId: Int,
 
     @ColumnInfo(name = "channel_name")
-    val channelName: String?,
+    val channelName: String,
 
     @ColumnInfo(name = "upvote")
-    val upVotes: Int?,
+    val upVotes: Int,
 
     @ColumnInfo(name = "downvote")
-    val downVotes: Int?,
+    val downVotes: Int,
 
     @ColumnInfo(name = "comment")
-    val comments: Int?,
+    val comments: Int,
 
     @ColumnInfo(name = "view")
-    val views: Int?
+    val views: Int
+)
+
+fun NewsEntity.toModel() = News(
+    id = this.id,
+    title = this.title,
+    coverImg = this.coverImg,
+    date = this.createdAt,
+    channelId = this.channelId,
+    channelName = this.channelName,
+    upVotes = this.upVotes,
+    downVotes = this.downVotes,
+    comments = this.comments,
+    views = this.views
 )
--- app/src/main/java/com/example/newsapp/data/local/room/NewsDao.kt
+++ app/src/main/java/com/example/newsapp/data/local/room/NewsDao.kt
@@ -5,7 +5,6 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.newsapp.data.local.entity.NewsEntity
-import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface NewsDao {
