b724bbaf1e99722934d25ba6f7bc8bf420b1ef6a
allantabriez <allantabriez@gmail.com>
2021-11-01 16:59:58 +0700

    Implemented online-offline caching in MeRepositoryImpl.kt

--- app/src/main/java/com/example/newsapp/data/MeRepositoryImpl.kt
+++ app/src/main/java/com/example/newsapp/data/MeRepositoryImpl.kt
@@ -1,18 +1,45 @@
 package com.example.newsapp.data
 
+import com.example.newsapp.data.local.entity.toModel
 import com.example.newsapp.domain.model.News
 import com.example.newsapp.domain.model.Profile
 import com.example.newsapp.domain.repository.MeRepository
+import com.example.newsapp.utils.ErrorCode
+import com.example.newsapp.utils.NetworkThrowable
 
 class MeRepositoryImpl(
     private val localSource: MeDataSource,
     private val remoteSource: MeDataSource,
 ) : MeRepository {
     override suspend fun getNews(): List<News> {
-        return remoteSource.getNews().map { it.toModel() }
+        return try {
+            val remoteResponse = remoteSource.getNews()
+            localSource.deleteCachedNews()
+            localSource.insertNews(remoteResponse.map { it.toEntity() })
+            remoteResponse.map { it.toModel() }
+        } catch (e: NetworkThrowable) {
+            if (e.errorCode == ErrorCode.Code401) throw e
+            else {
+                val localResponse = localSource.getCachedNews()
+                if (localResponse.isEmpty()) throw e
+                else localResponse.map { it.toModel() }
+            }
+        }
     }
 
     override suspend fun getProfile(): Profile {
-        return remoteSource.getProfile().toModel()
+        return try {
+            val remoteResponse = remoteSource.getProfile()
+            localSource.deleteCachedProfile()
+            localSource.insertProfile(remoteResponse.toEntity())
+            remoteResponse.toModel()
+        } catch (e: NetworkThrowable) {
+            if (e.errorCode == ErrorCode.Code401) throw e
+            else {
+                val localResponse = localSource.getCachedProfile()
+                if (localResponse.isEmpty()) throw e
+                else localResponse[0].toModel()
+            }
+        }
     }
 }
